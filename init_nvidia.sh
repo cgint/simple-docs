9417a8b9f1736c89bc9e55f4315bd9170173ff16
cgi <christian.gintenreiter@smarter-ecommerce.com>
2024-02-26 18:34:34 +0100

    simplify script

--- init_nvidia.sh
+++ init_nvidia.sh
@@ -6,11 +6,7 @@
 #  && mkdir dev && cd dev && git clone https://github.com/cgint/simple-docs.git && cd simple-docs \
 #  && echo "Now run either sh init_nvidia.sh or sh init_no_nvidia.sh"
 
-sudo apt install -y curl wget atop docker-compose docker-buildx net-tools python3.11 python3.11-venv python3-pip
-sudo apt autoremove -y && sudo apt clean all
-sudo usermod -a -G docker christian.gintenreiter
-sudo systemctl enable docker
-sudo service docker start
+sh init_no_nvidia.sh
 
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
     && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
@@ -19,11 +15,6 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
 sudo apt update && sudo apt upgrade -y && sudo apt install -y nvidia-container-toolkit nvidia-driver-545
 sudo systemctl daemon-reload
 sudo service docker restart
-
-python3.11 -m venv venv
-source venv/bin/activate
-venv/bin/pip install --upgrade pip
-venv/bin/pip install -r requirements.txt --upgrade
 
 echo "INIT: Ollama ..."
 curl -fsSL https://ollama.com/install.sh | sh
