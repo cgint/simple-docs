c6d89bb7cd8e04c4a6d133da81b5e36cf13258ca
cgi <christian.gintenreiter@smarter-ecommerce.com>
2024-03-14 10:42:21 +0100

    Use Groq with fallbacke to Together.ai

--- blabla_ask.sh
+++ blabla_ask.sh
@@ -19,14 +19,17 @@ fi
 #  LLM_ENGINE=ollama-ssh LLM_MODEL=neural-chat HOST_IP_OLLAMA=localhost \
 #  LLM_ENGINE=ollama-multi HOST_IP_OLLAMA=192.168.0.99 LLM_MODEL=neural-chat \
 #  LLM_ENGINE=ollama HOST_IP_OLLAMA=localhost LLM_MODEL=neural-chat \
-#  LLM_ENGINE=groq LLM_MODEL=mixtral-8x7b-32768 \
 #  LLM_ENGINE=ollama-multi-local-4 LLM_MODEL=neural-chat \
+#  LLM_ENGINE=together LLM_MODEL=mistral-together OPENAI_MODEL=mistralai/Mistral-7B-Instruct-v0.2 \
+#  LLM_ENGINE=groq LLM_MODEL=mixtral-8x7b-32768 \
 # QUERY_ENGINE_VARIANT=vector-graph-term-kggraph-docsum-bm25
 
 PARAM_COMMAND=ask QUERY_ENGINE_VARIANT=vector-graph-term-kggraph-docsum-bm25 HOST_IP=$HOST_IP_ADDR \
  DATA_PLAYGROUND=$PLAYGROUND \
- LLM_ENGINE=together LLM_MODEL=mistral-together OPENAI_MODEL=mistralai/Mistral-7B-Instruct-v0.2 \
+ LLM_ENGINE=groq-together LLM_MODEL=mixtral-8x7b-32768 OPENAI_MODEL=mistralai/Mixtral-8x7B-Instruct-v0.1 \
  RERANKER_MODEL=BAAI/bge-reranker-base RERANKER_K=10 \
+ WRAP_IN_SUB_QUESTION_ENGINE=false \
+ TOKENIZERS_PARALLELISM=true \
  DATA_DIR=./data \
  python3.11 simple_docs.py
 
