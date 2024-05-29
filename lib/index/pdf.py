d66a42f2b68cb36094eb11052d3ece2086f1d847
cgint <christian.gintenreiter@gmail.com>
2024-05-29 22:18:26 +0200

    Fix PDF reading

--- lib/index/pdf.py
+++ lib/index/pdf.py
@@ -1,9 +1,11 @@
 
 from typing import List
+from pathlib import Path
+
 from llama_index.core import Document
 from llama_index.readers.file import PDFReader
 
 def get_content_from_pdf_file(pdf_file_path: str) -> List[Document]:
     # https://docs.llamaindex.ai/en/stable/api_reference/readers.html
     loader = PDFReader()
-    return loader.load_data(file=pdf_file_path, extra_info={"source_id": pdf_file_path})
+    return loader.load_data(file=Path(pdf_file_path), extra_info={"source_id": pdf_file_path})
